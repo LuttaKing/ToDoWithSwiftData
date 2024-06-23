c37d2782ad8120718d51b578c1347f537f5a70ee
LuttaKing <denilsonwashuma@gmail.com>
2024-06-24 00:25:01 +0300

    Initial Commit

---
+++ ToDoWithSwiftData/ToDoWithSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoWithSwiftDataApp.swift
+//  ToDoWithSwiftData
+//
+//  Created by Denilson Washuma on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoWithSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
